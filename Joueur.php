3a456a5e92fcc403bbfd10e386a6cb36981f9afd
NoaDespaux <noa.despaux@etu.iut-tlse3.fr>
2022-12-09 10:06:32 +0100

    first commit

---
+++ Joueur.php
@@ -0,0 +1,28 @@
+<?php
+
+	class Joueur{
+
+		private $num_license;
+
+		public function __construct($num_license){
+			$this->num_license = $num_license;
+		}
+
+		public function getNomJoueur(){
+			try{
+				$bdd = new BDD();
+				$selectNom = $bdd->linkpdo->prepare('SELECT nom from joueur WHERE num_license = ?');
+		        $selectNom->execute(array($this->num_license));
+		        $nom = $selectNom->fetch();
+		        echo $nom;
+		        echo "connx ok";
+	    	} catch(Exception $e) {
+		        echo"erreur";
+		        die('Erreur:'.$e->getMessage());
+	    	}
+	    	return $nom;
+		}
+
+	}
+
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Equipe de hand</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+	<?php
+
+		include ("Connexion.php");
+	    include ("Joueur.php");
+
+	    $num = 2;
+
+	    $j = new Joueur($num);
+	    $j->getNomJoueur();
+
+	?>
+
+</body>
