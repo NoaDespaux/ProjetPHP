a8707d89ef6cc5ec3ec6bf361e500b83c91bd91d
JehanPhlp <93180306+JehanPhlp@users.noreply.github.com>
2022-12-09 10:25:17 +0100

    index

--- index.php
+++ index.php
@@ -19,7 +19,40 @@
 	    $j = new Joueur($num);
 	    $j->getNomJoueur();
 
+
 	?>
+
+	<table>
+		<thead>
+			<tr>
+				<th>Player Name</th>
+				<th>Position</th>
+				<th>Height (cm)</th>
+				<th>Weight (kg)</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Tommy Smith</td>
+				<td>Goalkeeper</td>
+				<td>195</td>
+				<td>92</td>
+			</tr>
+			<tr>
+				<td>John Doe</td>
+				<td>Right Wing</td>
+				<td>182</td>
+				<td>78</td>
+			</tr>
+			<tr>
+				<td>Jane Doe</td>
+				<td>Left Wing</td>
+				<td>176</td>
+				<td>65</td>
+			</tr>
+		</tbody>
+	</table>
+
 
 </body>
 
