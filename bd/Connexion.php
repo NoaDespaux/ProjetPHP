ae1d4331efba34d9c161300aa44a80a43f011eda
NoaDespaux <noa.despaux@etu.iut-tlse3.fr>
2023-01-21 16:34:03 +0100

    aa

--- bd/Connexion.php
+++ bd/Connexion.php
@@ -7,14 +7,7 @@
 	        $db = 'bd_php_hand';
 	        $login = 'root';
 	        $mdp = '';
-
-            /*
-            $server = 'localhost';
-            $db = 'phpdubbu_hand';
-            $login = 'phpdubbu_tmp3rt1v';
-            $mdp = '';
-            */
-
+            
             try {
                 $this->linkpdo = new PDO("mysql:host=$server;dbname=$db", $login, $mdp);
                 //$this->linkpdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
